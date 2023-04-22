b7bf9f643a0703ab7dabb6f720d8b1d772dc6291
cruway <astralkaiser89@gmail.com>
2023-04-22 09:44:04 +0900

    Initial commit

---
+++ src/main/java/practice/springbootoauth/common/config/QuerydslConfig.java
@@ -0,0 +1,16 @@
+package com.example.springsecurity.common.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+
+@Configuration
+public class QuerydslConfig {
+
+    @Bean
+    public JPAQueryFactory jpaQuery(EntityManager entityManager) {
+        return new JPAQueryFactory(entityManager);
+    }
+}
